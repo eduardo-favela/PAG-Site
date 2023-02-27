ca79bc30b8f7edf682524efc1908acd5f9a96cc7
Eduardo Favela <arturo99favela@gmail.com>
2023-02-27 12:11:32 -0600

    correccion de apikey

--- API/src/config.js
+++ API/src/config.js
@@ -2,8 +2,8 @@ import {config as dotenv} from 'dotenv';
 dotenv();
 
 export const config = {
-    host: process.env.DB_HOST || '200.100.100.50',
-    user:process.env.DB_USER || 'admin',
-    password: process.env.DB_PASS || '12345',
+    host: process.env.DB_HOST || 'alfagamma.com.mx',
+    user:process.env.DB_USER || 'sistemas_coord-web',
+    password: process.env.DB_PASS || 'p4gW3b!@',
     database: process.env.DB_DATABASE || 'pag_system'
 }
